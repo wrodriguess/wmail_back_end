fcfaf2df87e6204565ea1111dccb45999dbd0ac0
William <willrodriguess77@gmail.com>
2022-06-13 11:31:21 -0300

    Initializing the project

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'wmail',
+      user: 'postgres',
+      password: 'postgres'
+    }
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: `${__dirname}/src/database/migrations`
+  }
+
+};
