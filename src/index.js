e544fbbeeb3470c67469f6d3d6e3d1f0f4205915
William <willrodriguess77@gmail.com>
2022-06-13 14:01:54 -0300

    Adding treatment error on the server

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+// Not Found
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+
+// Catch All
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({error: error.message})
+})
+
 app.listen(3001, () => {
     console.log('API ON')
 })
